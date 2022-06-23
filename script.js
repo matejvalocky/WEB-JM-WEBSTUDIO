350a91b829fd7620fd7ef5e65d63f9942d53a44d
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2022-06-23 21:43:53 +0200

    pridanie side menu do stránky + naštýlovanie menu buttonu = menuBtn

---
+++ script.js
@@ -0,0 +1,9 @@
+// SIDE MENU ANIMATION************************************//
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
