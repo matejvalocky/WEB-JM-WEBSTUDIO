895d5cdcf5678ffaba8e05a655b814131c86fed9
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2022-08-08 21:38:01 +0200

    linkovanie scroll efektov

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ $(function(){
 
   $("#webstudio").click(function() {                          // plynuté presunutie do urcitej sekcie */
     $('html, body').animate({
-        scrollTop: $(".ponuknutWrapper").offset().top -90     // korekcia vertikalneho posunutia o pixely -90pixelov */
+        scrollTop: $("html, body").offset().top -90     // korekcia vertikalneho posunutia o pixely -90pixelov */
     }, 1000);
 });
 
@@ -25,9 +25,31 @@ $(function(){
 
 $(".textViacInfo").click(function() {                         // plynuté presunutie do urcitej sekcie */
   $('html, body').animate({
-      scrollTop: $("#now").offset().top +90                   // korekcia vertikalneho posunutia o pixely +90pixelov */
+      scrollTop: $("#overview").offset().top +900                   // korekcia vertikalneho posunutia o pixely +90pixelov */
   }, 1000);
 });
+
+
+
+$(".moreInfo").click(function() {                               // plynuté presunutie do urcitej sekcie */
+  $('html, body').animate({
+      scrollTop: $("#ceny").offset().top -90                     // korekcia vertikalneho posunutia o pixely +90pixelov */
+  }, 1000);
+});
+
+
+
+
+$(".zaujaliBtn").click(function() {                               // plynuté presunutie do urcitej sekcie */
+  $('html, body').animate({
+      scrollTop: $("#kontakt").offset().top -90                     // korekcia vertikalneho posunutia o pixely +90pixelov */
+  }, 1000);
+});
+
+
+
+
+
 
 
 
